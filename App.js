293d2803d177c04bbe4a2c7e1a2d316ec24fadec
Prashanta Baral <baralprashanta07@gmail.com>
2024-05-23 21:27:06 +0545

    Fix minor css issue and create constant file for data

--- App.js
+++ App.js
@@ -8,43 +8,19 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import MyComponent from './src/component/MyComponent';
+import {data} from './src/constant';
 
 function App() {
-  const data = [
-    {id: 1, name: 'John Doe'},
-    {id: 2, name: 'Jane Smith'},
-    {id: 3, name: 'Alice Johnson'},
-    {id: 4, name: 'Bob Williams'},
-    {id: 5, name: 'Eva Brown'},
-    {id: 6, name: 'Frank Davis'},
-    {id: 7, name: 'Grace Martinez'},
-    {id: 8, name: 'Henry Anderson'},
-    {id: 9, name: 'Ivy Taylor'},
-    {id: 10, name: 'Jack Lee'},
-  ];
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={styles.container}>
       <MyComponent data={data} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
   },
 });
 
---
+++ src/constant/index.js
@@ -0,0 +1,12 @@
+export const data = [
+  {id: 1, name: 'John Doe'},
+  {id: 2, name: 'Jane Smith'},
+  {id: 3, name: 'Alice Johnson'},
+  {id: 4, name: 'Bob Williams'},
+  {id: 5, name: 'Eva Brown'},
+  {id: 6, name: 'Frank Davis'},
+  {id: 7, name: 'Grace Martinez'},
+  {id: 8, name: 'Henry Anderson'},
+  {id: 9, name: 'Ivy Taylor'},
+  {id: 10, name: 'Jack Lee'},
+];
